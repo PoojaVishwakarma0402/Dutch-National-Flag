b9bd8ba054285445e03c82df2b348ec7861abd8d
PoojaVishwakarma0402 <123650050+PoojaVishwakarma0402@users.noreply.github.com>
2023-10-26 02:01:05 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void sort012(int arr[], int n) {
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[mid], arr[low]);
+            mid++;
+            low++;
+        } else if (arr[mid] == 1) {
+            mid++;
+        } else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {1, 0, 2, 1, 0, 2, 1, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    sort012(arr, n);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+       cout << arr[i] << " ";
+    }
+       cout << std::endl;
+
+    return 0;
+}
